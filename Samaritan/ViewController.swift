5330ad252c4aca58a370ebd160eea983e327bba1
Franklin Byaruhanga <byaruhaf@appbantu.com>
2022-09-12 03:53:13 +0300

    Added Basic WKWebView for testing

--- Samaritan/ViewController.swift
+++ Samaritan/ViewController.swift
@@ -6,12 +6,23 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let url = URL(string: "https://stil.kurir.rs/moda/157971/ovo-su-najstilizovanije-zene-sveta-koja-je-po-vama-br-1-anketa")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
