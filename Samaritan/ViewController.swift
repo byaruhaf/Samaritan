3be99ab4abb911271a91b9f641e9ed846a1b3821
Franklin Byaruhanga <byaruhaf@appbantu.com>
2022-09-12 03:15:30 +0300

    Initial Commit

---
+++ Samaritan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Samaritan
+//
+//  Created by Franklin Byaruhanga on 12/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
